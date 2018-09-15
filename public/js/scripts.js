8b9adf4d9f18cd0849737901d9f34c522a9ebdaf
Pham Minh <smpleader@gmail.com>
2018-09-15 07:17:16 +0700

    finished footer

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -14,6 +14,19 @@ $(document).ready(function(){
     $('.slick-sliders').slick({
         autoplay:true
     });
+    $('.slick-stories').slick({
+        autoplay:false,
+        /*slidesPerRow:3,*/
+        centerPadding: '10px',
+        slidesToShow:3,
+        slide:".item",
+        responsive: [
+            {
+                breakpoint: 480,
+                slidesToShow:1
+            }
+        ]
+    });
 });;
 $(document).ready(function(){
     $(document).on("mouseenter tap click",".green",function(){
--- src/js/custom/slick-setup.js
+++ src/js/custom/slick-setup.js
@@ -3,4 +3,17 @@ $(document).ready(function(){
     $('.slick-sliders').slick({
         autoplay:true
     });
+    $('.slick-stories').slick({
+        autoplay:false,
+        /*slidesPerRow:3,*/
+        centerPadding: '10px',
+        slidesToShow:3,
+        slide:".item",
+        responsive: [
+            {
+                breakpoint: 480,
+                slidesToShow:1
+            }
+        ]
+    });
 });
