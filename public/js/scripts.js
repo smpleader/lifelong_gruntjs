ddfdbe2b0304aea9002a0717403be1411570c4c7
Pham Minh <smpleader@gmail.com>
2018-09-26 08:37:07 +0700

    add fixed for footer

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -713,12 +713,6 @@ $(document).ready(function(){
       topSpacing:0,
       zIndex:1
     });
-   // var topspacing =  $(window).height() - $("#foot-free-app").height(); console.log(topspacing);
-    $("#foot-free-app").sticky({
-      topSpacing: topspacing ,
-      bottomSpacing:0,
-      zIndex:1
-    });
   });;
 $(document).ready(function(){
     $(document).on("mouseenter tap click","#top-readmores .green",function(){
