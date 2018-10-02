a81f5ac3b53dd9e7ea306005fd304f606803b2d2
Pham Minh <smpleader@gmail.com>
2018-10-02 15:58:58 +0700

    add pregnant-types-of-adoption.html

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -634,26 +634,6 @@ if ( typeof define === 'function' && define.amd ) {
   });
 }));
 ;
-var place_img_for_lifelong_staff = function(){
-    var wd = $( window ).width();
-    if( wd > 1326 ){
-        $("#lifelong-staff .content").show();
-        $("#lifelong-staff #img-content").appendTo( $("#desktop-img-position"));
-    }else if( wd > 480){
-        $("#lifelong-staff .content").show();
-        $("#lifelong-staff #img-content").appendTo( $("#tablet-img-position"));
-    }else{
-        $("#lifelong-staff #img-content").appendTo( $("#mobile-img-position"));
-        $("#lifelong-staff .content").hide();
-    }
-}
-$(document).ready( function(){
-    place_img_for_lifelong_staff();
-    $("#lifelong-staff a.readmore").click(function(){
-        $("#lifelong-staff .content").toggle();
-    });    
-});
-$( window ).resize(function(){ place_img_for_lifelong_staff() });;
 $(document).ready(function(){
     var canvasMenu =  new mlPushMenu( 
         document.getElementById( 'mp-menu' ), 
