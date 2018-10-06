3b8341061b92a67edef964dba380a213b9d87fd1
Pham Minh <smpleader@gmail.com>
2018-10-07 00:55:44 +0700

    add pregnant-faq.html

--- src/js/custom/callbox-setup.js
+++ src/js/custom/callbox-setup.js
@@ -2,15 +2,10 @@
 
 var set_callbox_dimension = function(){
     $(".callbox .bg").each(function(){
-        par = $(this).parents(".callbox");
-        extra = 0;
-        if(par.hasClass("topleft")) extra = 48;
-        if(par.hasClass("topright")) extra = 48;
-        if(par.hasClass("bottomleft")) extra = 53;
-        if(par.hasClass("bottomright")) extra = 53;
+        var par = $(this).parents(".callbox");
         $(this).height( par.innerHeight() -8 ); 
-        // console.log($(this),par.children(".content").outerHeight(),$(this).height());
-        $(this).width( par.innerWidth() - 8); //console.log($(this).width() );
+        // console.log(  par.innerHeight() , $(this).height());
+        $(this).width( par.innerWidth() - 8);
     });
 }
 $(document).ready( function(){
